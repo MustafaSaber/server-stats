0b178bd6610b6b79a895214ae0f18f2fbe39b496
Mostafa Saber <mostafa.saber3789@gmail.com>
2021-05-25 23:51:27 +0200

    Fix paths

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -4,4 +4,4 @@ PRIV_KEY=${1:-private_key.pem}
 
 echo $PRIV_KEY
 
-ansible-playbook ./../ansible/playbook.yml -i ./../ansible/hosts --private-key=$PRIV_KEY
+ansible-playbook ./ansible/playbook.yml -i ./ansible/hosts --private-key=$PRIV_KEY
