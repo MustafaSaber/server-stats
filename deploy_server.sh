da0082b9b26f74cc198964822c21902b877c3c97
Mostafa Saber <mostafa.saber3789@gmail.com>
2021-05-25 23:46:42 +0200

    Refactor structure 1

--- deploy_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PRIV_KEY=${1:-private_key.pem}
-
-echo $PRIV_KEY
-
-ansible-playbook playbook.yml -i ./hosts --private-key=$PRIV_KEY
---
+++ scripts/deploy_server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+PRIV_KEY=${1:-private_key.pem}
+
+echo $PRIV_KEY
+
+ansible-playbook ./../ansible/playbook.yml -i ./../ansible/hosts --private-key=$PRIV_KEY
